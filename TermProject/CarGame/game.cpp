0332bc8903a7078479666dae3703eebd78150a2e
Muhammet kilinc <mhmmtklnc07@gmail.com>
2024-05-10 15:01:49 +0300

    new files

--- TermProject/CarGame/game.cpp
+++ TermProject/CarGame/game.cpp
@@ -7,51 +7,67 @@
 #include <queue>
 #include <ctime>
 #include <string.h>
-#define wHeight 40 // height of the road
-#define wWidth 100 // width of the road
-#define lineX 45 // x coordinate of the middle line
-#define lineLEN 10 // distance of the middle line from the beginning and the end
-#define EXITY 35 // coordinate showing the end of the road
-#define leftKeyArrow 260 // ASCII code of the left arrow key
-#define RightKeyArrow 261 // ASCII code of the right arrow key
-#define leftKeyA 97// ASCII code of A
-#define RightKeyD 100 // ASCII code of D
-#define ESC 27 // // ASCII code of the ESC key
-#define ENTER 10 // ASCII code of the ENTER key
-#define KEYPUP 259 // ASCII code of the up arrow key
-#define KEYDOWN 258 // ASCII code of the down arrow key
-#define KEYERROR -1 // ASCII code returned if an incorrect key is pressed
-#define SAVEKEY 115 // ASCII code of S
-#define levelBound 300 // To increase level after 300 points
-#define MAXSLEVEL 5 // maximum level
-#define ISPEED 500000 // initial value for game moveSpeed
-#define DRATESPEED 100000 // to decrease moveSpeed after each new level
-#define MINX 5 // minimum x coordinate value when creating cars
-#define MINY 10 // the maximum y coordinate value when creating the cars, then we multiply it by -1 and take its inverse
-#define MINH 5 // minimum height when creating cars
-#define MINW 5 // minimum width when creating cars
-#define SPEEDOFCAR 3 // speed of the car driven by the player
-#define YOFCAR 34 // y coordinate of the car used by the player
-#define XOFCAR 45 // x coordinate of the car used by the player
-#define IDSTART 10 // initial value for cars ID
-#define IDMAX 20// maximum value for cars ID
-#define COLOROFCAR 3 // color value of the car used by the player
-#define POINTX 91 //x coordinate where the point is written
-#define POINTY 42 //y coordinate where the point is written
-#define MENUX 10 // x coordinate for the starting row of the menus
-#define MENUY 5 // y coordinate for the starting row of the menus
-#define MENUDIF 2 // difference between menu rows
-#define MENUDIFX 20 // difference between menu columns
-#define MENSLEEPRATE 200000 // sleep time for menu input
-#define GAMESLEEPRATE 250000 // sleep time for player arrow keys
-#define EnQueueSleep 1 // EnQueue sleep time
-#define DeQueueSleepMin 2 // DeQueue minimum sleep time
-#define numOfcolors 4 // maximum color value that can be selected for cars
-#define maxCarNumber 5 // maximum number of cars in the queue
-#define numOfChars 3 // maximum number of patterns that can be selected for cars
-#define settingMenuItem 2 // number of options in the setting menu
-#define mainMenuItem 6 // number of options in the main menu
+
+// Ekranın boyutları
+#define wHeight 40 // Yolun yüksekliği
+#define wWidth 100 // Yolun genişliği
+
+// Orta çizgi ve bitiş koordinatları
+#define lineX 45 // Ortadaki çizginin x koordinatı
+#define lineLEN 10 // Orta çizginin başlangıc ve sonundan uzaklığı
+#define EXITY 35 // Yolun bitiş koordinatı
+
+// Klavye girdileri
+#define leftKeyArrow 260 // Sol ok tuşunun ASCII kodu
+#define RightKeyArrow 261 // Sağ ok tuşunun ASCII kodu
+#define leftKeyA 97 // A'nın ASCII kodu
+#define RightKeyD 100 // D'nin ASCII kodu
+#define ESC 27 // ESC tuşunun ASCII kodu
+#define ENTER 10 // ENTER tuşunun ASCII kodu
+#define KEYPUP 259 // Yukarı ok tuşunun ASCII kodu
+#define KEYDOWN 258 // Aşağı ok tuşunun ASCII kodu
+#define KEYERROR -1 // Yanlış bir tuşa basıldığında döndürülen ASCII kodu
+#define SAVEKEY 115 // S'nin ASCII kodu
+
+// Seviye ve hız sınırları
+#define levelBound 300 // 300 puanı geçince seviyeyi artırmak için
+#define MAXSLEVEL 5 // Maksimum seviye
+#define ISPEED 500000 // Oyun hareket hızı için başlangıç değeri
+#define DRATESPEED 100000 // Her yeni seviyeden sonra hareket hızını azaltmak için
+
+// Araba özellikleri
+#define MINX 5 // Arabaların oluşturulurken minimum x koordinat değeri
+#define MINY 10 // Arabaların oluşturulurken maksimum y koordinat değeri
+#define MINH 5 // Arabaların oluşturulurken minimum yükseklik değeri
+#define MINW 5 // Arabaların oluşturulurken minimum genişlik değeri
+#define SPEEDOFCAR 3 // Oyuncu tarafından kullanılan arabanın hızı
+#define YOFCAR 34 // Oyuncu tarafından kullanılan arabanın y koordinatı
+#define XOFCAR 45 // Oyuncu tarafından kullanılan arabanın x koordinatı
+#define IDSTART 10 // Araba kimliği için başlangıç değeri
+#define IDMAX 20 // Araba kimliği için maksimum değer
+#define COLOROFCAR 3 // Oyuncu tarafından kullanılan arabanın renk değeri
+#define POINTX 91 // Puanın yazıldığı x koordinatı
+#define POINTY 42 // Puanın yazıldığı y koordinatı
+
+// Menü özellikleri
+#define MENUX 10 // Menülerin başlangıç satırı için x koordinatı
+#define MENUY 5 // Menülerin başlangıç satırı için y koordinatı
+#define MENUDIF 2 // Menü satırları arasındaki fark
+#define MENUDIFX 20 // Menü sütunları arasındaki fark
+#define MENSLEEPRATE 200000 // Menü giriş için bekleme süresi
+#define GAMESLEEPRATE 250000 // Oyuncu ok tuşları için bekleme süresi
+
+// Diğer sabitler
+#define EnQueueSleep 1 // EnQueue bekleme süresi
+#define DeQueueSleepMin 2 // DeQueue minimum bekleme süresi
+#define numOfcolors 4 // Arabalar için seçilebilecek maksimum renk değeri
+#define maxCarNumber 5 // Kuyruktaki maksimum araba sayısı
+#define numOfChars 3 // Arabalar için seçilebilecek maksimum desen sayısı
+#define settingMenuItem 2 // Ayarlar menüsündeki seçenek sayısı
+#define mainMenuItem 6 // Ana menüdeki seçenek sayısı
 using namespace std;
+
+
 typedef struct Car{ //
     int ID;
     int x;
@@ -78,7 +94,6 @@ typedef struct Game{
     int points;
 }Game;
 
-
 Game playingGame; // Global variable used for new game
 const char *gameTxt =  "game.txt";
 const char *CarsTxt =  "cars.txt";
@@ -92,11 +107,13 @@ void printWindow(); //Draws the road on the screen
 void *newGame(void *); // manages new game
 void initGame(); // Assigns initial values to all control parameters for the new game
 void initWindow(); //Creates a new window and sets I/O settings
+
+
 int main()
 {
     playingGame.leftKey = leftKeyArrow;
     playingGame.rightKey = RightKeyArrow;
-    initGame();
+    initGame(); // ekran baslatildi
     initWindow();
     pthread_t th1; //create new thread
     pthread_create(&th1, NULL, newGame,NULL);// Run newGame function with thread
@@ -104,6 +121,8 @@ int main()
     return 0;
 }
 
+
+// init metodu ellenmicek
 void initGame()
 {
     playingGame.cars = queue<Car>();
@@ -123,9 +142,12 @@ void initGame()
     playingGame.current.clr = COLOROFCAR;
     playingGame.current.chr = '*';
 }
+
+// new game baslar baslamaz yolu ciziyor
+// kullanıcı
 void *newGame(void *)
 {
-    printWindow();
+    printWindow(); 
     drawCar(playingGame.current,2,1); // Draw the car the player is driving on the screen
     int key;
     while (playingGame.IsGameRunning) { //continue until the game is over
@@ -137,7 +159,7 @@ void *newGame(void *)
                         drawCar(playingGame.current,2,1); // draw player's car with new position
                 }
             }
-         usleep(GAMESLEEPRATE); // sleep
+         usleep(GAMESLEEPRATE); // sleep  0.25 sn de bir gerceklestirecek
         }
 }
 void initWindow()
@@ -148,7 +170,7 @@ void initWindow()
 	nodelay(stdscr, true);// set the getch() function to non-blocking mode
 	curs_set(0);          // hide the cursor
 	cbreak();             // disable line buffering
-	noecho();             // don't echo characters entered by the user
+	noecho();             // don't echo characters entered by the user, ekrana a ya da d yazilmamasi icin
 	clear();              // clear the screen
     sleep(1);
 }
@@ -157,7 +179,7 @@ void printWindow()
     for (int i = 1; i < wHeight - 1; ++i) {
 		//mvprintw: Used to print text on the window, paramters order: y , x , string
         mvprintw(i, 2, "*"); //left side of the road
-        mvprintw(i, 0, "*");
+        mvprintw(i, 0, "*"); 
         mvprintw(i, wWidth - 1, "*");// right side of the road
         mvprintw(i, wWidth - 3, "*");
     }
@@ -166,20 +188,23 @@ void printWindow()
     }
 }
 
+
+// type 2 oldugu zaman ciz , 1 oldugu zaman sil
 void drawCar(Car c, int type, int direction )
 {
 	//If the user does not want to exit the game and the game continues
     if(playingGame.IsSaveCliked!=true && playingGame.IsGameRunning==true)
     {
+
             init_pair(c.ID, c.clr, 0);// Creates a color pair: init_pair(short pair ID, short foregroundcolor, short backgroundcolor);
-            //0: Black (COLOR_BLACK)
-			//1: Red (COLOR_RED)
-			//2: Green (COLOR_GREEN)
-			//3: Yellow (COLOR_YELLOW)
-			//4: Blue (COLOR_BLUE)
+                                                //0: Black (COLOR_BLACK)
+                                                //1: Red (COLOR_RED)
+                                                //2: Green (COLOR_GREEN)
+                                                //3: Yellow (COLOR_YELLOW)
+                                                //4: Blue (COLOR_BLUE)
 			attron(COLOR_PAIR(c.ID));//enable color pair
             char drawnChar;
-            if (type == 1 )
+            if (type == 1 ) 
                drawnChar = ' '; // to remove car
             else
                drawnChar= c.chr; //  to draw char
@@ -192,7 +217,7 @@ void drawCar(Car c, int type, int direction )
             mvhline(c.y, c.x, drawnChar, c.width);// top line of rectangle
             mvhline(c.y + c.height - 1, c.x, drawnChar, c.width); //bottom line of rectangle
             if(direction == 0) // If it is any car on the road
-                mvhline(c.y + c.height, c.x, drawnChar, c.width);
+                mvhline(c.y + c.height, c.x, drawnChar, c.width); 
             else //player's card
                 mvhline(c.y -1, c.x, drawnChar, c.width);
 		    //mvvline: used to draw a vertical line in the window
@@ -205,7 +230,7 @@ void drawCar(Car c, int type, int direction )
             mvvline(c.y, c.x + c.width - 1, drawnChar, c.height); //right line of rectangle
             char text[5];
             if (type == 1 )
-                sprintf(text,"  "); //to remove point
+                sprintf(text,"  "); //to remove point 
             else
                  sprintf(text,"%d",c.height * c.width); // to show car's point in rectangle
             mvprintw(c.y+1, c.x +1, text);// display car's point in rectangle
